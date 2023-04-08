fba04ef831ebeef37028c02f907e709a0f3d37e0
hooriya <fkfahadkhan900@gmail.com>
2023-04-08 21:05:45 +0500

    practice task

--- app.js
+++ app.js
@@ -1,3 +1,152 @@
+// Day 01 5 April
+// alert("Thanks for your input!");
+
+
+// var name = "Mark";
+// console.log(name);
+// name = "Ace";
+// console.log(name);
+
+
+// var nationality;
+// nationality = "U.S.";
+
+// var floogle = "Mark";
+// console.log(floogle);
+
+// var lessonAuthor = "Mark";
+// var guyWhoKeepsSayingHisOwnName = "Mark";
+// var x = "Mark";
+// console.log(lessonAuthor);
+// console.log(guyWhoKeepsSayingHisOwnName);
+// console.log(x);
+
+// var lastName = "Smith";
+// var cityOfOrigin = "New Orleans";
+// var aussieGreeting = "g'Day";
+// console.log(lastName);
+
+// var thanx = "Thanks for your input!"
+//  alert(thanx);
+
+
+// var weight = 150;
+// console.log(weight);
+// console.log(weight+25);
+
+
+// var originalNum = 23;
+// var newNum = originalNum + 7;
+//  console.log(originalNum);
+//  console.log(newNum);
+
+
+//  var originalNum = 23;
+//  var numToBeAdded = 7;
+//  var newNum = originalNum + numToBeAdded;
+ 
+//  var originalNum = "23";
+// var newNum = originalNum + 7;
+
+//  console.log(newNum);
+
+
+//  var 1stPresident = "Washington";
+//   var prezWhoCame1st = "Washington";
+//  console.log(prezWhoCame1st);
+
+
+// var caseQty = 144;
+// alert(caseQty);
+
+//  alert(144);
+
+
+// var popularNumber = 4;
+// var popularNumber = 2 + 2;
+// alert(2 + 2);
+
+//  console.log(popularNumber);
+
+
+//  var popularNumber = 12 - 24;
+//  console.log(popularNumber);
+//   var popularNumber = 3 * 12;
+// console.log(popularNumber);
+
+// var num = 10;
+//  var popularNumber = num + 200;
+//  console.log(popularNumber);
+
+
+// 
+
+// var num = 1;
+// var newNum =num++;
+//  var num = 1;
+// var newNum = ++num;
+// console.log(num);
+
+
+// var num = 1;
+//  var newNum = num--;
+//  console.log(num);
+
+
+
+//  var num = 1;
+//  var newNum = --num;
+//  console.log(num);
+
+
+// var totalCost = 1 + 3 * 4;
+// console.log(totalCost);
+
+// var totalCost = 1 + (3 * 4);
+// console.log(totalCost);
+
+// var totalCost = (1 + 3) * 4;
+// console.log(totalCost);
+
+// var resultOfComputation = (2 * 4) * 4 + 2;
+// console.log(resultOfComputation);
+
+// resultOfComputation = (2 * 4) * (4 + 2);
+// console.log(resultOfComputation);
+
+
+// var message = "Thanks for your input!";
+// alert(message);
+
+
+// alert("Thanks, " + userName + "!");
+
+// var message = "Thanks, ";
+// var banger = "!";
+// alert(message + userName + banger);
+
+
+
+// var message = "Thanks, ";
+//  var userName = "Susan";
+//  var banger = "!";
+//  var customMess = message + userName + banger;
+//  alert(customMess)
+
+
+
+// alert("2" + "2");
+
+// alert("2" + "2");
+
+// var spec = prompt("Your species?", "human");
+
+//  var numberOfCats = prompt("How many cats?");
+//  var tooManyCats = numberOfCats + 1;
+
+
+// day 7 april zakat task
+
 var zakatPercentage = 0.025;
 var userInput = Number( prompt("Enter a number"));
 var result = (zakatPercentage) * (userInput);
